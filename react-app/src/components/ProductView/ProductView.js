10b9005123997f1c0c2b90a2c677e8590532100d
midknightsurfer <beau@hirethedeveloper.com>
2022-06-13 22:09:46 -0500

    fixing deployment

--- react-app/src/components/ProductView/ProductView.js
+++ react-app/src/components/ProductView/ProductView.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getProducts } from "../../store/products";
 import ReactBnbGallery from "react-bnb-gallery";
 
 import "react-bnb-gallery/dist/style.css";
 
 const ProductView = () => {
   const { productId } = useParams();
+  const dispatch = useDispatch();
 
   const product = useSelector((state) => state.products[productId]);
 
   const [photoIndex, setPhotoIndex] = useState(0);
   const [photoObject, setPhotoObject] = useState([]);
   const [showPhotoModal, setShowPhotoModal] = useState(false);
+
+  useEffect(() => {
+    dispatch(getProducts(productId));
+  }, [dispatch]);
 
   const handlePhotos = (photosIndex) => {
     if (!photoObject.length) {
@@ -30,35 +36,41 @@ const ProductView = () => {
     setShowPhotoModal(true);
   };
 
-
   return (
     <div className="productview__container">
       <div className="productview__images-container">
-      <div className="productview__img-maincontainer" style={{backgroundImage: `url(${product.images[0]})`}}>
-      </div>
-      <div className="productview__img-gallery">
-          {product.images.map(image => (
-            <div className="productview__image-container" onClick={() => handlePhotos()} style={{backgroundImage: `url(${image})`}}>           
-            </div>
+        <div
+          className="productview__img-maincontainer"
+          style={{ backgroundImage: `url(${product?.images[0]})` }}
+        ></div>
+        <div className="productview__img-gallery">
+          {product?.images.map((image) => (
+            <div
+              className="productview__image-container"
+              onClick={() => handlePhotos()}
+              style={{ backgroundImage: `url(${image})` }}
+            ></div>
           ))}
-      </div> 
-      <ReactBnbGallery
-        show={showPhotoModal}
-        onClose={() => setShowPhotoModal(false)}
-        photos={photoObject}
-        activePhotoIndex={photoIndex}
-      />    
+        </div>
+        <ReactBnbGallery
+          show={showPhotoModal}
+          onClose={() => setShowPhotoModal(false)}
+          photos={photoObject}
+          activePhotoIndex={photoIndex}
+        />
       </div>
 
       <div className="productview__information-container">
-        <h2>{product.title}</h2>
-        <p>{product.description}</p>
-        <h5>Price: ${product.price}.00</h5>
-        <h5>Shipping: ${product.shipping_price}.00</h5>
+        <h2>{product?.title}</h2>
+        <p>{product?.description}</p>
+        <div className="add_to_cart_container">
+          <h4>Add to Cart</h4>
+          <h5>Price: ${product?.price}.00</h5>
+          <h5>Shipping: ${product?.shipping_price}.00</h5>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default ProductView
+export default ProductView;
